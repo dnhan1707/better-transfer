d72cc7b11b8baaff886c222392d00f6959d46f05
Nhan Tri Danh <57444770+dnhan1707@users.noreply.github.com>
2025-07-13 19:29:34 -0700

    update cors

--- app/main.py
+++ app/main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
 from app.api.routes.transfer import create_transfer_router
+from fastapi.middleware.cors import CORSMiddleware
 
 
 def create_application() -> FastAPI:
     app = FastAPI()
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["http://localhost:3000"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"]
+    )
     transfer_router = create_transfer_router()
     app.include_router(transfer_router)
     return app
